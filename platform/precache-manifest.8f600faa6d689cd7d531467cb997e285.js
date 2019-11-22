5ab6c57716863281edf9a73c8a649471490fdfc5
Scott Montford <scottmontford@gmail.com>
2019-11-22 22:56:17 +0000

    Platform Build

---
+++ platform/precache-manifest.8f600faa6d689cd7d531467cb997e285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde723ef64f0ec962fe7fdda85cc903b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf61984f6baca444276",
+    "url": "/static/css/main.1d25f35b.chunk.css"
+  },
+  {
+    "revision": "62f8662439ccadf82367",
+    "url": "/static/js/2.2a125cb6.chunk.js"
+  },
+  {
+    "revision": "4bf61984f6baca444276",
+    "url": "/static/js/main.9bb6264d.chunk.js"
+  },
+  {
+    "revision": "fac1ebc9ffdc8a30cb79",
+    "url": "/static/js/runtime-main.e3e2d2a6.js"
+  }
+]);
