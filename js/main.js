63d60d43973253fd208f5b055b37f78326b39949
Scott Montford <scottmontford@gmail.com>
2020-02-08 16:06:25 +0000

    Coming Soon Page
    Coming Soon Page Loaded Into Webserver.

--- js/main.js
+++ js/main.js
@@ -1,211 +1,397 @@
-"use strict";
-jQuery(document).ready(function() {
-	//menu
-	if (jQuery().superfish) {
-		jQuery('ul.sf-menu').superfish({
-			delay:       700,
-			animation:   {opacity:'show',height:'show'},
-			//animation:   {opacity:'show'},
-			animationOut: {opacity: 'hide'},
-			speed:       'fast',
-			disableHI:   false,
-			cssArrows:   false,
-			autoArrows:  true
-		});
-	}
+/*-----------------------------------------------------------------------------------
+/*
+/* Main JS
+/*
+-----------------------------------------------------------------------------------*/  
 
-	//toTop
-	if (jQuery().UItoTop) {
-        jQuery().UItoTop({ easingType: 'easeOutQuart' });
-    }
+(function($) {
 
-    //horizontal accordion
-    if (jQuery().elastislide) {
-	    jQuery('#horizontal_slider').elastislide({
-	        imageW : 360,
-	        border : 0,
-	        minItems : 1,
-	        margin : 30
-	    });
-	}
+   /*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
 
-    //prettyPhoto
-    if (jQuery().prettyPhoto) {
-	   	jQuery("a[rel^='prettyPhoto']").prettyPhoto({
-			theme: 'facebook' /* light_rounded / dark_rounded / light_square / dark_square / facebook / pp_default*/
-	  	});
-	}
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
 
-   	//carousel
-   	if (jQuery().carousel) {
-		jQuery('.carousel').carousel();
-	}
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
 
-	//single page localscroll and scrollspy
-	var navHeight = jQuery('#header').outerHeight(true) + 80;
-	jQuery('body').scrollspy({
-		target: '.mainmenu_wrap',
-		offset: navHeight
-	});
-	if (jQuery().localScroll) {
-		jQuery('#mainmenu').localScroll({
-			duration:1900,
-			easing:'easeOutQuart',
-			offset: -40
-		});
-		jQuery('#header').localScroll({
-			duration:1900,
-			easing:'easeOutQuart',
-			offset: -40
-		});
-	}
+      });     
 
-	//portfolio and horizontal slider animation
-	jQuery('.portfolio_item_image .portfolio_links').css({opacity: 0});
-	jQuery('.isotope-item, .horizontal_slider_introimg').hover(
-	 	function() {
-			jQuery( this ).find('.portfolio_item_image .portfolio_links').stop().animate({ opacity: 1}, 500, 'easeOutExpo').find('.p-view').toggleClass('moveFromLeft').end().find('.p-link').toggleClass('moveFromRight');
-		}, function() {
-			jQuery( this ).find('.portfolio_item_image .portfolio_links').stop().animate({ opacity: 0}, 300, 'easeOutExpo').find('.p-view').toggleClass('moveFromLeft').end().find('.p-link').toggleClass('moveFromRight');
-		}
-	);
+  	})
 
-	//teaser style5 animation
-	jQuery('.single_teaser.icons.style5').hover(
-	 	function() {
-			jQuery( this ).find('i').addClass('moveFromLeft').end().find('h3').addClass('moveFromRight').end().find('p').addClass('moveFromBottom');
-		}, function() {
-			jQuery( this ).find('i').removeClass('moveFromLeft').end().find('h3').removeClass('moveFromRight').end().find('p').removeClass('moveFromBottom');
-		}
-	);
+   /*---------------------------------------------------- */
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2017/01/01';
 
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
 
-	//twitter
-	if (jQuery().tweet) {
-		jQuery('.twitter').tweet({
-			modpath: "./twitter/",
-		    count: 3,
-		    avatar_size: 48,
-		    loading_text: 'loading twitter feed...',
-		    join_text: 'auto',
-		    username: 'ThemeForest', 
-		    template: "{avatar}{time}{join}<span class=\"tweet_text\">{tweet_text}</span>"
-		});
-	}
+   		$(this).html(event.strftime('<span>%D <em>days</em></span>' + 
+   										 	 '<span>%H <em>hours</em></span>' + 
+   										 	 '<span>%M <em>minutes</em></span>' +
+   										 	 '<span>%S <em>seconds</em></span>'));
 
-});
+   });
 
-jQuery(window).load(function(){
-	
-	setTimeout(function(){
-		jQuery('.progress-bar').addClass('stretchRight');
-	}, 600);
-
-	//stick header to top
-	if (jQuery().sticky) {
-	    jQuery("#header").sticky({ 
-	    		topSpacing: 0,
-	    		scrollBeforeStick: 150
-	    	},
-	    	function(){ 
-	    		jQuery("#header").stop().animate({opacity:0}, 0).delay(500).stop().animate({opacity:1}, 1000);
-	    	},
-	       	function(){ 
-	    		jQuery("#header").stop().animate({opacity:0}, 0).delay(50).stop().animate({opacity:1}, 2000);
-	    	}
-	    );
-	}
+   /*----------------------------------------------------*/
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */  	 
+	$('input').placeholder() 
 	
 
-	if (jQuery().flexslider) {
-		var $mainSlider = jQuery('#mainslider');
-		jQuery(".flexslider").flexslider({
-			animation: "fade",
-			useCSS: true,
-			controlNav: true,   
-			//animationLoop: false,
-			smoothHeight: true,
-			slideshowSpeed:5000,
-			animationSpeed:800,
-			after :function( slider ){
-			  	//bg-color1 - class for #mainslider
-			  	var currentClass = $mainSlider.find('.flex-active-slide').attr('data-bg');
-			  	$mainSlider.attr('class', currentClass);
-			}
-		});
-	}
+   /*----------------------------------------------------- */
+   /* Modals
+   ------------------------------------------------------- */   
+   $('.modal-toggles ul').on('click', 'a', function(e) {
 
-	jQuery('body').delay(1000).scrollspy('refresh');
+   	var html = $('html'),
+   		 main = $('main, footer'),
+   		 footer = $('footer'),           
+          curMod = $(this).attr('href'),  
+          modal = $(curMod),
+          modClose = modal.find('#modal-close');          
+         
+		main.fadeOut(500, function(){
+			$('html,body').scrollTop(0);
+        	modal.addClass('is-visible');
+      });  
+      
+      e.preventDefault();
+
+      // for old ie
+      if (html.hasClass('oldie')) {
+
+      	$(document).on('click', "#modal-close", function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+      	});
+
+      }
+      // other browsers
+      else {
+
+      	modClose.on('click', function(evt) {
+	      	$('html,body').scrollTop(0); 
+	      	modal.removeClass('is-visible');
+	      	setTimeout(function() {      
+	        		main.fadeIn(500); 
+	        	}, 500);       
+	        	        
+	        	evt.preventDefault();
+	      });
+
+      }     	
+
+   });
+
+   /*---------------------------------------------------- */
+	/* Owl Carousel
+	------------------------------------------------------ */ 
+	$("#owl-slider").owlCarousel({
+        navigation: false,
+        pagination: true,
+        items: 4,
+        navigationText: false
+    });
 
 
-	//preloader
-	jQuery(".preloaderimg").fadeOut();
-	jQuery(".preloader").delay(200).fadeOut("slow").delay(200, function(){
-		jQuery(this).remove();
+   /*----------------------------------------------------*/
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	  $('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
+
+  	}, 100);
+
+
+   /*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
 	});
 
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'We have sent you a confirmation email',
+	//  1: 'Please enter a value',
+	//  2: 'An email address must contain a single @',
+	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
 
-	//flickr
-	if (jQuery().jflickrfeed) {
-		jQuery("#flickr").jflickrfeed({
-			flickrbase: "http://api.flickr.com/services/feeds/",
-			limit: 6,
-			qstrings: {
-				id: "54297118@N03"
-			},
-			itemTemplate: '<a href="{{image_b}}" rel="prettyPhoto[pp_gal]"><li><img alt="{{title}}" src="{{image_s}}" /></li></a>'
-		}, function(data) {
-			jQuery("#flickr a").prettyPhoto({
-				theme: 'facebook'
-	   		});
-	   		jQuery("#flickr li").hover(function () {						 
-			   jQuery(this).find("img").stop().animate({ opacity: 0.5 }, 200);
-		    }, function() {
-			   jQuery(this).find("img").stop().animate({ opacity: 1.0 }, 400);
-		    });
-		});
+	$.ajaxChimp.translations.es = {
+	  'submit': 'Submitting...',
+	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
 	}
 
-});
+	/*---------------------------------------------------- */
+	/* Map
+	------------------------------------------------------ */
+	var latitude = 14.549072,
+		 longitude = 121.046958,
+		 map_zoom = 15,		 
+		 main_color = '#d8ac00',
+		 saturation_value= -30,
+		 brightness_value= 5,
+		 winWidth = $(window).width();		 
 
-jQuery(window).resize(function(){
-	jQuery("#header").sticky('update');
-	jQuery('body').scrollspy('refresh');
+   // marker url
+	if ( winWidth > 480 ) {
+		marker_url = 'images/icon-location-b.png';                    
+   } else {
+      marker_url = 'images/icon-location.png';            
+   }	 
 
-});
+	// map style
+	var style = [ 
+		{
+			// set saturation for the labels on the map
+			elementType: "labels",
+			stylers: [
+				{ saturation: saturation_value }
+			]
+		},  
+	   {	// poi stands for point of interest - don't show these lables on the map 
+			featureType: "poi",
+			elementType: "labels",
+			stylers: [
+				{visibility: "off"}
+			]
+		},
+		{
+			// don't show highways lables on the map
+	      featureType: 'road.highway',
+	      elementType: 'labels',
+	      stylers: [
+	         { visibility: "off" }
+	      ]
+	   }, 
+		{ 	
+			// don't show local road lables on the map
+			featureType: "road.local", 
+			elementType: "labels.icon", 
+			stylers: [
+				{ visibility: "off" } 
+			] 
+		},
+		{ 
+			// don't show arterial road lables on the map
+			featureType: "road.arterial", 
+			elementType: "labels.icon", 
+			stylers: [
+				{ visibility: "off" }
+			] 
+		},
+		{
+			// don't show road lables on the map
+			featureType: "road",
+			elementType: "geometry.stroke",
+			stylers: [
+				{ visibility: "off" }
+			]
+		}, 
+		// style different elements on the map
+		{ 
+			featureType: "transit", 
+			elementType: "geometry.fill", 
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		}, 
+		{
+			featureType: "poi",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "poi.government",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "poi.sport_complex",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "poi.attraction",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "poi.business",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "transit",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "transit.station",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "landscape",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+			
+		},
+		{
+			featureType: "road",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		},
+		{
+			featureType: "road.highway",
+			elementType: "geometry.fill",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		}, 
+		{
+			featureType: "water",
+			elementType: "geometry",
+			stylers: [
+				{ hue: main_color },
+				{ visibility: "on" }, 
+				{ lightness: brightness_value }, 
+				{ saturation: saturation_value }
+			]
+		}
+	];
+		
+	// map options
+	var map_options = {
 
-jQuery(window).scroll(function() {
+      	center: new google.maps.LatLng(latitude, longitude),
+      	zoom: 15,
+      	panControl: false,
+      	zoomControl: false,
+        	mapTypeControl: false,
+      	streetViewControl: false,
+      	mapTypeId: google.maps.MapTypeId.ROADMAP,
+      	scrollwheel: false,
+      	styles: style
 
-	//circle progress bar
-	if (jQuery().easyPieChart) {
-		var count = 0 ;
-		var colors = ['#a8d164', '#fbcf61', '#ff6f6f'];
-		jQuery('.chart').each(function(){
+    	};
 
-				
-			var imagePos = jQuery(this).offset().top;
-			var topOfWindow = jQuery(window).scrollTop();
-			if (imagePos < topOfWindow+600) {
+   // inizialize the map
+	var map = new google.maps.Map(document.getElementById('map-container'), map_options);
 
-				jQuery(this).easyPieChart({
-			        barColor: colors[count],
-					trackColor: '#343434',
-					scaleColor: false,
-					scaleLength: false,
-					lineCap: 'butt',
-					lineWidth: 10,
-					size: 130,
-					rotate: 0,
-					animate: 2000,
-					onStep: function(from, to, percent) {
-							jQuery(this.el).find('.percent').text(Math.round(percent));
-						}
-			    });
-			}
+	// add a custom marker to the map				
+	var marker = new google.maps.Marker({
 
-			count++;
-			if (count >= colors.length) { count = 0};
+		 	position: new google.maps.LatLng(latitude, longitude),
+		 	map: map,
+		 	visible: true,
+		 	icon: marker_url
+		 
 		});
+
+	// add custom buttons for the zoom-in/zoom-out on the map
+	function CustomZoomControl(controlDiv, map) {
+	
+		// grap the zoom elements from the DOM and insert them in the map 
+	 	var controlUIzoomIn= document.getElementById('map-zoom-in'),
+		  	 controlUIzoomOut= document.getElementById('map-zoom-out');
+
+		controlDiv.appendChild(controlUIzoomIn);
+		controlDiv.appendChild(controlUIzoomOut);
+
+		// Setup the click event listeners and zoom-in or out according to the clicked element
+		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+			map.setZoom(map.getZoom()+1)
+		});
+		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+			map.setZoom(map.getZoom()-1)
+		});
+			
 	}
 
-});
+	var zoomControlDiv = document.createElement('div');
+	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+	// insert the zoom div on the top right of the map
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(zoomControlDiv);	
+
+
+
+})(jQuery);
