dfe692629a361e70d69e2bfb21283eff7fcbd110
Scott Montford <scottmontford@gmail.com>
2021-05-24 02:13:40 +0100

    Pricing Rework
    New Pricing Structure
    New Pricing Style
    References Added
    Discount Codes Added

--- js/calculator.js
+++ js/calculator.js
@@ -1,57 +1,47 @@
 function calculator() {
 
-    var websiteCost = 300.0;
+    var websiteCost = 450.0;
     var pageCost = 8.00;
     var contentCost = 60.00;
     var blogCost = 60.00;
     var analyticsCost = 25.00;
-    var adsenseCost = 25.00;
-    var socialMediaCost = 25.00;
     var hostCost = 6.00;
-    var bookingCost = 20.00;
     var domainFee = 10.00;
-    var liveChatcost = 5.00;
-    var ecommerceCost = 250.00;
-    var creditDisc = 5.00;
-    var cmsCost = 4.00;
-    var discount = 10.00;
+    var liveChatcost = 10.00;
+    var ecommerceCost = 280.00;
+    var creditDisc = 10.00;
+    var mainLow = 10.00;
+    var mainMed = 15.00;
+    var mainHigh = 25.00;
 
     var pageQty;
     var contentQty;
     var blogQty;
     var analyticsQty;
-    var adsenseQty;
-    var socialMediaQty;
     var hostQty;
-    var bookingQty;
     var domainQty;
     var livechatQty;
     var ecommerceQty;
     var creditQty;
-    var cmsQty;
 
     var pageRes;
     var contentRes;
     var blogRes;
     var analyticsRes;
-    var adsenseRes;
-    var socialMediaRes;
     var hostRes;
-    var bookingRes;
     var domainRes;
     var liveChatRes;
     var ecommerceRes;
     var creditRes;
-    var ecommerceMthly;
-    var cmsRes;
+    var mainRes;
 
     var initRes;
     var resMonthly;
     var resYearly;
     var deposit;
-    var discountTot;
 
     pageQty = (document.pricingcalc.txtPages.value);
+
     if (document.getElementById("contentCreation").value == "Yes") {
         contentQty = 1;
     } else {
@@ -67,22 +57,6 @@ function calculator() {
     } else {
         analyticsQty = 0;
     }
-    if (document.getElementById("adsenseSupport").value == "Yes") {
-        adsenseQty = 1;
-    } else {
-        adsenseQty = 0;
-    }
-    socialMediaQty = (document.pricingcalc.txtSocialMedia.value);
-    if (document.getElementById("hostSupport").value == "Yes") {
-        hostQty = 1;
-    } else {
-        hostQty = 0;
-    }
-    if (document.getElementById("bookingSupport").value == "Yes") {
-        bookingQty = 1;
-    } else {
-        bookingQty = 0;
-    }
     if (document.getElementById("domainSupport").value == "Yes") {
         domainQty = 1;
     } else {
@@ -94,57 +68,67 @@ function calculator() {
     else {
         livechatQty = 0;
     }
+    //Level of Maintanence
+    if (document.getElementById("maintain").value == "low") {
+        mainRes = mainLow;
+    }
+    if (document.getElementById("maintain").value == "med") {
+        mainRes = mainMed;
+    }
+    if (document.getElementById("maintain").value == "high") {
+        mainRes = mainHigh;
+    }
+    if (document.getElementById("maintain").value == "Select") {
+        mainRes = 0;
+    }
+
+
+
+    if (document.getElementById("hostSupport").value == "Yes") {
+        hostQty = 1;
+    } else {
+        hostQty = 0;
+    }
     if (document.getElementById('ecommerceSupport').value == "Yes") {
         ecommerceQty = 1;
-        ecommerceMthly = 5;
     }
     else {
         ecommerceQty = 0;
-        ecommerceMthly = 0;
     }
     if (document.getElementById("creditSupport").value == "Yes") {
         creditQty = 1;
     } else {
         creditQty = 0;
     }
-    if (document.getElementById("cmsSupport").value == "Yes") {
-        cmsQty = 1;
+
+    //Discount Codes
+    if (document.getElementById("discountCode").value == "LAUNCH50") {
+        websiteCost = 200.00;
     } else {
-        cmsQty = 0;
-    }
-
-
-    if (document.getElementById("cmsSupport") == "Yes" && (document.getElementById("hostSupport") == "Yes"))  {
-        cmsCost = 2.00;
+        websiteCost = 400.00;
     }
 
     pageRes = (pageCost * pageQty) + (1.5 * pageQty);
     contentRes = contentCost * contentQty;
     blogRes = blogCost * blogQty;
     analyticsRes = analyticsCost * analyticsQty;
-    adsenseRes = adsenseCost * adsenseQty;
-    socialMediaRes = socialMediaCost * socialMediaQty;
     hostRes = hostCost * hostQty;
-    bookingRes = bookingCost * bookingQty;
     domainRes = domainFee * domainQty;
     liveChatRes = liveChatcost * livechatQty;
     ecommerceRes = ecommerceCost * ecommerceQty;
     creditRes = creditDisc * creditQty;
-    cmsRes = cmsCost * cmsQty;
     
 
 
 
-    initRes = websiteCost + pageRes + contentRes + blogRes + analyticsRes + adsenseRes
-                + socialMediaRes + hostRes + bookingRes + liveChatRes + ecommerceRes + cmsRes + domainRes - creditRes;
-    resMonthly = hostRes + bookingRes + + cmsRes + ecommerceMthly;
-    resYearly = resMonthly * 12 - (resMonthly*12*0.15);
+    initRes = websiteCost + pageRes + contentRes + blogRes + analyticsRes + hostRes + liveChatRes + ecommerceRes + domainRes - creditRes;
+    resMonthly = hostRes + mainRes;
+    resYearly = resMonthly * 12 - 12;
     deposit = initRes / 2;
-    discountTot = initRes - (discount * initRes / 100);
 
-    console.log("Discount Price: " + discountTot);
-    document.getElementById("initResult").innerHTML = "Full Quote: £ " + initRes + " + ";
-    document.getElementById("resMonthly").innerHTML = "Monthly £ " + resMonthly + " or ";
-    document.getElementById("resYearly").innerHTML = "Yearly: £ " + resYearly;
+    document.getElementById("initResult").innerHTML = "Full Payment: £ " + initRes + " + ";
+    document.getElementById("resMonthly").innerHTML = "£" + resMonthly + " Per Month" + " (£ " + resYearly + " Yearly)";
     document.getElementById("deposit").innerHTML = "Deposit: £ " + deposit;
+    document.getElementById("reference").innerHTML = "Quote Reference: " + Date.now();
+    document.getElementById("help").innerHTML = "If you are happy with your quote, quote your reference here!";
 }
--- js/calculatorland.js
+++ js/calculatorland.js
@@ -1,29 +1,27 @@
 function calculator() {
 
-    var websiteCost = 50.0;
+    var websiteCost = 150.0;
     var analyticsCost = 25.00;
-    var adsenseCost = 25.00;
     var hostCost = 6.00;
     var domainFee = 10.00;
-    var liveChatcost = 5.00;
-    var creditDisc = 5.00;
-    var cmsCost = 4.00;
+    var liveChatcost = 10.00;
+    var creditDisc = 10.00;
+    var mainLow = 5.00;
+    var mainMed = 10.00;
+    var mainHigh = 15.00;
 
     var analyticsQty;
-    var adsenseQty;
     var hostQty;
     var domainQty;
     var livechatQty;
     var creditQty;
-    var cmsQty;
 
     var analyticsRes;
-    var adsenseRes;
     var hostRes;
     var domainRes;
     var liveChatRes;
     var creditRes;
-    var cmsRes;
+    var mainRes;
 
     var initRes;
     var resMonthly;
@@ -34,16 +32,6 @@ function calculator() {
         analyticsQty = 1;
     } else {
         analyticsQty = 0;
-    }
-    if (document.getElementById("adsenseSupport").value == "Yes") {
-        adsenseQty = 1;
-    } else {
-        adsenseQty = 0;
-    }
-    if (document.getElementById("hostSupport").value == "Yes") {
-        hostQty = 1;
-    } else {
-        hostQty = 0;
     }
     if (document.getElementById("domainSupport").value == "Yes") {
         domainQty = 1;
@@ -56,40 +44,56 @@ function calculator() {
     else {
         livechatQty = 0;
     }
+    //Level of Maintanence
+    if (document.getElementById("maintain").value == "low") {
+        mainRes = mainLow;
+    }
+    if (document.getElementById("maintain").value == "med") {
+        mainRes = mainMed;
+    }
+    if (document.getElementById("maintain").value == "high") {
+        mainRes = mainHigh;
+    }
+    if (document.getElementById("maintain").value == "Select") {
+        mainRes = 0.00;
+    }
+
+    if (document.getElementById("hostSupport").value == "Yes") {
+        hostQty = 1;
+    } else {
+        hostQty = 0;
+    }
+
     if (document.getElementById("creditSupport").value == "Yes") {
         creditQty = 1;
     } else {
         creditQty = 0;
     }
-    if (document.getElementById("cmsSupport").value == "Yes") {
-        cmsQty = 1;
+
+    //Discount Codes
+    if (document.getElementById("discountCode").value == "LANDING50") {
+        websiteCost = 50.00;
+        document.getElementById("savings").innerHTML = "Savings: £100";
     } else {
-        cmsQty = 0;
-    }
-
-
-    if (document.getElementById("cmsSupport") == "Yes" && (document.getElementById("hostSupport") == "Yes"))  {
-        cmsCost = 2.00;
+        websiteCost = 150;
+        document.getElementById("savings").innerHTML = "Savings: £0";
     }
 
     analyticsRes = analyticsCost * analyticsQty;
-    adsenseRes = adsenseCost * adsenseQty;
     hostRes = hostCost * hostQty;
     domainRes = domainFee * domainQty;
     liveChatRes = liveChatcost * livechatQty;
     creditRes = creditDisc * creditQty;
-    cmsRes = cmsCost * cmsQty;
     
 
-
-
-    initRes = websiteCost + analyticsRes + adsenseRes + hostRes + liveChatRes + cmsRes + domainRes - creditRes;
-    resMonthly = hostRes + cmsRes;
-    resYearly = resMonthly * 12 - (resMonthly*12*0.15);
+    initRes = websiteCost + analyticsRes + hostRes + liveChatRes + domainRes - creditRes;
+    resMonthly = hostRes + mainRes;
+    resYearly = resMonthly * 12 - 12;
     deposit = initRes / 2;
 
-    document.getElementById("initResult").innerHTML = "Full Quote: £ " + initRes + " + ";
-    document.getElementById("resMonthly").innerHTML = "Monthly £ " + resMonthly + " or ";
-    document.getElementById("resYearly").innerHTML = "Yearly: £ " + resYearly;
+    document.getElementById("initResult").innerHTML = "Full Payment: £ " + initRes + " + ";
+    document.getElementById("resMonthly").innerHTML = "£" + resMonthly + " Per Month" + " (£ " + resYearly + " Yearly)";
     document.getElementById("deposit").innerHTML = "Deposit: £ " + deposit;
+    document.getElementById("reference").innerHTML = "Quote Reference: " + Date.now();
+    document.getElementById("help").innerHTML = "If you are happy with your quote, quote your reference here!";
 }
