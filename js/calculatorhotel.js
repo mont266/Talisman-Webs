4e3a9d4358a5a77e4f6473ed46e22c221f6df1d7
Scott Montford <scottmontford@gmail.com>
2021-09-22 12:49:16 +0100

    Hotel Pricing Added
    -Pricing calc for hotel

--- js/calculatorhotel.js
+++ js/calculatorhotel.js
@@ -1,40 +1,30 @@
 function calculator() {
 
-    var websiteCost = 400.0;
-    var pageCost = 8.00;
-    var contentCost = 60.00;
-    var blogCost = 60.00;
+    var websiteCost = 801.25;
     var analyticsCost = 25.00;
     var hostCost = 6.00;
     var domainFee = 10.00;
     var liveChatcost = 10.00;
-    var ecommerceCost = 300.00;
-    var ecommerceMonCost = 12.00;
     var creditDisc = 10.00;
     var mainLow = 10.00;
     var mainMed = 15.00;
     var mainHigh = 25.00;
 
-    var pageQty;
-    var contentQty;
-    var blogQty;
+    var pro = false;
+
+
+    var roomQty;
     var analyticsQty;
     var hostQty;
     var domainQty;
     var livechatQty;
-    var ecommerceQty;
-    var ecommerceMonQty;
     var creditQty;
 
-    var pageRes;
-    var contentRes;
-    var blogRes;
+    var roomRes;
     var analyticsRes;
     var hostRes;
     var domainRes;
     var liveChatRes;
-    var ecommerceRes;
-    var ecommerceMonRes;
     var creditRes;
     var mainRes;
 
@@ -43,21 +33,7 @@ function calculator() {
     var resYearly;
     var deposit;
 
-    pageQty = (document.pricingcalc.txtPages.value);
-
-    //Content Creation
-    if (document.getElementById("contentCreation").value == "Yes") {
-        contentQty = 1;
-    } else {
-        contentQty = 0;
-    }
-
-    //Blog
-    if (document.getElementById("blogSupport").value == "Yes") {
-        blogQty = 1;
-    } else {
-        blogQty = 0;
-    }
+    roomQty = (document.pricingcalc.txtPages.value);
 
     //Analytics
     if (document.getElementById("analyticsSupport").value == "Yes") {
@@ -79,6 +55,13 @@ function calculator() {
     }
     else {
         livechatQty = 0;
+    }
+    
+    //Booking.com Integration
+    if (document.getElementById('bookingCom').value == "Yes") {
+        pro = true;
+    } else {
+        pro = false;
     }
 
     //Level of Maintanence
@@ -102,17 +85,6 @@ function calculator() {
         hostQty = 0;
     }
 
-    //eCommerce
-    if (document.getElementById('ecommerceSupport').value == "Yes") {
-        ecommerceQty = 1;
-        ecommerceMonQty = 1;
-        hostQty = 0;
-    }
-    else {
-        ecommerceQty = 0;
-        ecommerceMonQty = 0;
-    }
-
     //Credit Discount
     if (document.getElementById("creditSupport").value == "Yes") {
         creditQty = 1;
@@ -122,82 +94,123 @@ function calculator() {
 
         //Discount Codes
         if (document.getElementById("discountCode").value == "LAUNCH50") {
-            websiteCost = 225.00;
-            Document.getElementById("savings").innerHTML = "Savings: £225"
+            websiteCost = 250.00;
         } else {
-            websiteCost = 450.00;
+            websiteCost = 800.00;
         }
     
         if (document.getElementById("discountCode").value == "LAUNCH25") {
-            websiteCost = 337.50;
+            websiteCost = 375.00;
         } else {
-            websiteCost = 450.00;
+            websiteCost = 800.00;
         }
     
         if (document.getElementById("discountCode").value == "CAUSEWAY") {
             websiteCost = 360.00;
             pageCost = 6.40;
-            contentCost = 48.00;
-            blogCost = 48.00;
             analyticsCost = 20.00;
             hostCost = 4.80;
             domainFee = 8.00;
             liveChatcost = 8.00;
-            ecommerceCost = 240.00;
-            ecommerceMonCost = 8.00;
         } else {
-            websiteCost = 450.00;
+            websiteCost = 800.00;
             pageCost = 8.00;
-            contentCost = 60.00;
-            blogCost = 60.00;
             analyticsCost = 25.00;
             hostCost = 6.00;
             domainFee = 10.00;
             liveChatcost = 10.00;
-            ecommerceCost = 300.00;
-            ecommerceMonCost = 12.00;
         }
 
         if (document.getElementById("discountCode").value == "Ref") {
             websiteCost = 382.50;
             pageCost = 6.80;
-            contentCost = 51.00;
-            blogCost = 51.00;
             analyticsCost = 21.25;
             hostCost = 5.10;
             domainFee = 8.50;
             liveChatcost = 8.50;
-            ecommerceCost = 255.00;
-            ecommerceMonCost = 10.20;
         } else {
-            websiteCost = 450.00;
+            websiteCost = 800.00;
             pageCost = 8.00;
-            contentCost = 60.00;
-            blogCost = 60.00;
             analyticsCost = 25.00;
             hostCost = 6.00;
             domainFee = 10.00;
             liveChatcost = 10.00;
-            ecommerceCost = 300.00;
-            ecommerceMonCost = 12.00;
         }
 
-    pageRes = (pageCost * pageQty) + (1.5 * pageQty);
-    contentRes = contentCost * contentQty;
-    blogRes = blogCost * blogQty;
+    //1-2 Rooms
+    if (roomQty < 3 ) {
+        if (pro == true) {
+            roomRes = 25.00;
+        } else {
+            roomRes = 7.00;
+        }
+    }
+
+    //3-5 Rooms
+    if (roomQty > 2 && roomQty < 6 ) {
+        if (pro == true) {
+            roomRes = 35.00;
+        } else {
+            roomRes = 12.00;
+        }
+    }
+
+    //6-10 Rooms
+    if (roomQty > 5 && roomQty < 11 ) {
+        if (pro == true) {
+            roomRes = 50.00;
+        } else {
+            roomRes = 15.00;
+        }
+    }
+
+    //11-20 Rooms
+    if (roomQty > 10 && roomQty < 21 ) {
+        if (pro == true) {
+            roomRes = 65.00;
+        } else {
+            roomRes = 19.00;
+        }
+    }
+
+    //21-50 Rooms
+    if (roomQty > 20 && roomQty < 51 ) {
+        if (pro == true) {
+            roomRes = 79.00;
+        } else {
+            roomRes = 25.00;
+        }
+    }
+
+    //50-100 Rooms
+    if (roomQty > 50 && roomQty < 101 ) {
+        if (pro == true) {
+            roomRes = 99.00;
+        } else {
+            roomRes = 35.00;
+        }
+    }
+
+    //100+ Rooms
+    if (roomQty > 100 ) {
+        if (pro == true) {
+            roomRes = 119.00;
+        } else {
+            roomRes = 45.00;
+        }
+    }
+
     analyticsRes = analyticsCost * analyticsQty;
     hostRes = hostCost * hostQty;
     domainRes = domainFee * domainQty;
     liveChatRes = liveChatcost * livechatQty;
-    ecommerceRes = ecommerceCost * ecommerceQty;
-    ecommerceMonRes = ecommerceMonCost * ecommerceMonQty;
     creditRes = creditDisc * creditQty;
     
 
 
 
-    initRes = websiteCost + pageRes + contentRes + blogRes + analyticsRes + hostRes + liveChatRes + ecommerceRes + domainRes - creditRes;
-    resMonthly = hostRes + mainRes + ecommerceMonRes;
+    initRes = websiteCost + roomRes + analyticsRes + hostRes + liveChatRes + domainRes - creditRes;
+    resMonthly = hostRes + mainRes + roomRes;
     resYearly = resMonthly * 12 - 12;
     deposit = initRes / 2;
 
@@ -205,13 +218,4 @@ function calculator() {
     document.getElementById("resMonthly").innerHTML = "£" + resMonthly + " Per Month" + " (£ " + resYearly + " Yearly)";
     document.getElementById("deposit").innerHTML = "Deposit: £ " + deposit;
     document.getElementById("ref").innerHTML = "Quote Reference: " + Date.now();
-    document.getElementById("help").innerHTML = "If you are happy with your quote, Speak to us here!";
-}
-
-function Check() {
-    if (document.getElementById("name").value == "") {
-        
-    } else {
-        calculator();
-    }
 }
