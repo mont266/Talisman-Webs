79aafae54f71a681a015628bc59425cca4db967c
Scott Montford <scottmontford@gmail.com>
2021-11-21 21:28:12 +0000

    Calc pricing change

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,7 @@
 //Full Website
 function calculate1() {
 
-    var websiteCost = 450.0;
+    var websiteCost = 250.0;
     var pageCost = 8.00;
     var contentCost = 60.00;
     var blogCost = 60.00;
@@ -12,8 +12,8 @@ function calculate1() {
     var ecommerceCost = 300.00;
     var ecommerceMonCost = 12.00;
     var creditDisc = 10.00;
-    var mainLow = 10.00;
-    var mainMed = 15.00;
+    var mainLow = 6.00;
+    var mainMed = 12.00;
     var mainHigh = 25.00;
 
     var pageQty;
@@ -150,15 +150,15 @@ function calculate1() {
 //Landing Page
 function calculate2() {
 
-    var websiteCost = 250.0;
+    var websiteCost = 150.0;
     var analyticsCost = 25.00;
     var hostCost = 6.00;
     var domainFee = 10.00;
     var liveChatcost = 10.00;
     var creditDisc = 10.00;
-    var mainLow = 5.00;
-    var mainMed = 10.00;
-    var mainHigh = 15.00;
+    var mainLow = 6.00;
+    var mainMed = 12.00;
+    var mainHigh = 25.00;
     var cmsCost = 40.00;
 
     var analyticsQty;
@@ -259,8 +259,8 @@ function calculate3() {
     var domainFee = 10.00;
     var liveChatcost = 10.00;
     var creditDisc = 10.00;
-    var mainLow = 10.00;
-    var mainMed = 15.00;
+    var mainLow = 6.00;
+    var mainMed = 12.00;
     var mainHigh = 25.00;
 
     var pro = false;
